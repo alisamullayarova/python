cde8b767e34629caaeb269455254eb56dce7e0a9
alisamullayarova <126970620+alisamullayarova@users.noreply.github.com>
2023-11-05 23:25:27 +0500

    Add files via upload

---
+++ buns/mod6/task3.py
@@ -0,0 +1,24 @@
+def is_armstrong_number(number):
+    
+    total = 0
+    num_digits = len(str(number))
+    temp = number
+    
+    while temp > 0:
+        digit = temp % 10
+        total += digit ** num_digits
+        temp //= 10
+
+    return number == total
+
+def get_armstrong_numbers():
+    number = 10
+    while True:
+        if is_armstrong_number(number):
+            yield number
+        number += 1
+
+armstrong_generator = get_armstrong_numbers()
+
+for i in range(8):
+    print(next(armstrong_generator), end=' ') 
