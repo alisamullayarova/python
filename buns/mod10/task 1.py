31e64d30401e9c6e999ada7c58fdee430dd61106
alisamullayarova <126970620+alisamullayarova@users.noreply.github.com>
2023-12-03 00:05:21 +0500

    Add files via upload

---
+++ buns/mod10/task 1.py
@@ -0,0 +1,65 @@
+import requests
+import json
+
+output = {}
+
+def get_object_info(url):
+    response = requests.get(url)
+    data = json.loads(response.content)
+    return data
+
+def get_starships_info(url):
+    starships = get_object_info(url)['results']
+    return starships
+
+def get_planet_info(url):
+    planet = get_object_info(url)
+    return planet
+
+def find_Millennuim_Falcon(starships):
+    for starship in starships:
+        if starship['name'] == 'Millennium Falcon':
+            return starship
+        
+def print_starship_info(starship):
+    print('name: ', starship['name'])
+    print('starship_class: ', starship['starship_class'])
+    print('max_atmosphering_speed: ', starship['max_atmosphering_speed'])
+    output['name'] = starship['name']
+    output['starship_class'] = starship['starship_class']
+    output['max_atmosphering_speed'] = starship['max_atmosphering_speed']
+    print_pilots_info(starship['pilots'])
+
+def print_pilots_info(starship_pilots):
+    output['pilots'] = []
+    print('pilots: ')
+    for pilot in starship_pilots:
+            info = get_object_info(pilot)
+            pilot_info = {}
+            planet_name = get_planet_info(info['homeworld'])['name']
+            print('\tname: ', info['name'])
+            print('\theight: ', info['height'])
+            print('\tmass: ', info['mass'])
+            print('\thomeworld: ', planet_name)
+            print('\thomeworld_url: ', info['homeworld'])
+            print()
+            pilot_info['name'] = info['name']
+            pilot_info['height'] = info['height']
+            pilot_info['mass'] = info['mass']
+            pilot_info['homeworld'] = planet_name
+            pilot_info['homeworld_url'] = info['homeworld']
+            output['pilots'].append(pilot_info)
+
+def save_json():
+    with open('task1_Mullayarova.json', 'w') as outfile:
+        json.dump(output, outfile)
+
+def main():
+    starships = get_starships_info('https://swapi.dev/api/starships/')
+    Millennium_Falcon = find_Millennuim_Falcon(starships)
+    print_starship_info(Millennium_Falcon)
+    save_json()
+
+if __name__ == '__main__':
+    main()
+
